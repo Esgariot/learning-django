aa9b932f9b1e3c14e2a0ce50850b8501f4d1f05c
Esgariot <roberutsu@gmail.com>
2016-09-26 20:08:44 +0200

    dodana baza

--- tango_django/rango/models.py
+++ tango_django/rango/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
 	"""Question model for DB"""
-	question_text = models.models.CharField(max_length=200)
+	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
