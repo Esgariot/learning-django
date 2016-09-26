2ecbdffb4dac3bf01671b624a776d7f3a61c63ea
Esgariot <roberutsu@gmail.com>
2016-09-26 20:20:05 +0200

    added __str__ methods

--- tango_django/rango/models.py
+++ tango_django/rango/models.py
@@ -4,10 +4,14 @@ class Question(models.Model):
 	"""Question model for DB"""
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.question_text
 
 class Choice(models.Model):
 	"""docstring for Choice"""
 	question = models.ForeignKey(Question,
 		on_delete = models.CASCADE)
 	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)	
+	votes = models.IntegerField(default=0)
+	def __str__():
+		return self.choice_text	
