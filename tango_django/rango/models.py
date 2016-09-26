2e7d8bab3b6c71017df867ea77e8f49936b8e480
Esgariot <roberutsu@gmail.com>
2016-09-26 19:53:12 +0200

    kolejne zmiany tym razem nie wiem co :o

--- tango_django/rango/models.py
+++ tango_django/rango/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+	"""Question model for DB"""
+	question_text = models.models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	"""docstring for Choice"""
+	question = models.ForeignKey(Question,
+		on_delete = models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)	
